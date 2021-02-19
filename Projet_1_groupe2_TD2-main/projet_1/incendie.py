df95030eef5e88659f5d235fa51b7b4dc26f3fda
Martin PEREZ-CENIT <martin.perez-cenit@ens.uvsq.fr>
2021-02-19 11:35:14 +0100

    incendie.py

--- Projet_1_groupe2_TD2-main/projet_1/incendie.py
+++ Projet_1_groupe2_TD2-main/projet_1/incendie.py
@@ -9,3 +9,33 @@
 #MPCI TD2
 #dépot Github:
 #https://github.com/uvsq22001055/Projet_1_groupe2_TD2.git
+
+
+import tkinter as tk
+
+COTE = 10
+HAUTEUR = 600
+LARGEUR = 1000
+COULEUR_QUADR = "white"
+
+def quadrillage():
+    """Affiche un quadrillage constitué de carrés de côté COTE"""
+    y = 0
+    while y <= HAUTEUR:
+        canvas.create_line((0, y), (LARGEUR, y), fill=COULEUR_QUADR)
+        y += COTE
+    x = 0
+    while x <= LARGEUR:
+        canvas.create_line((x, 0), (x, HAUTEUR), fill=COULEUR_QUADR)
+        x += COTE
+    
+
+racine = tk.Tk()
+racine.title("incendie")
+
+canvas = tk.Canvas(racine, height = HAUTEUR, width = LARGEUR, bg = "blue")
+canvas.grid()
+
+quadrillage()
+
+racine.mainloop()
